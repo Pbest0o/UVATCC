ba73ea57d5522e6ad7a3a486b135251cce2c1d23
Pedro Martire <pmartire@pmartir-ltm3am9.internal.salesforce.com>
2022-06-23 17:14:39 -0300

    first commit

---
+++ src/main/java/com/tccuva1/tccuva/TccuvaApplication.java
@@ -0,0 +1,13 @@
+package com.tccuva1.tccuva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccuvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccuvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tccuva1/tccuva/TccuvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tccuva1.tccuva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccuvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
