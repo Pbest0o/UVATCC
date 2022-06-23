b8db34fe53d3a7239dfaedcd4f5ec4ed204a4ad4
Pedro Martire <pmartire@pmartir-ltm3am9.internal.salesforce.com>
2022-06-23 17:32:02 -0300

    3 commit

--- src/main/java/com/tccuva1/tccuva/TccuvaApplication.java
+++ src/main/java/com/tccuva1/tccuva/TccuvaApplication.java
@@ -2,10 +2,17 @@ package com.tccuva1.tccuva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class TccuvaApplication {
 
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TccuvaApplication.class, args);
 	}
