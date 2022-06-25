f24d057e44e00c71d8c1a2d6f8294c7fb7443103
Pedro Martire <pmartire@pmartir-ltm3am9.internal.salesforce.com>
2022-06-25 12:47:10 -0300

    commit 11

--- src/main/java/com/tccuva1/tccuva/TccuvaApplication.java
+++ src/main/java/com/tccuva1/tccuva/TccuvaApplication.java
@@ -2,19 +2,10 @@ package com.tccuva1.tccuva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 public class TccuvaApplication {
-
-
-	@GetMapping("/")
-	String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "";
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TccuvaApplication.class, args);
