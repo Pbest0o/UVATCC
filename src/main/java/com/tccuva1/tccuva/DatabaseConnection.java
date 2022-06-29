008daabbe1d39ebb52849b5f517801340d1e59a6
Pedro Martire <pmartire@pmartir-ltm3am9.internal.salesforce.com>
2022-06-29 01:21:36 -0300

    commit 14

--- src/main/java/com/tccuva1/tccuva/DatabaseConnection.java
+++ src/main/java/com/tccuva1/tccuva/DatabaseConnection.java
@@ -6,7 +6,7 @@ public class DatabaseConnection {
 
 
     public static void createConnection() throws ClassNotFoundException{
-        String url = "postgres://lirbgwsmobmsbb:45a31ccead123ab78ba6ef0a719744999157d94ef5c0ea69703c05d81095fc62@ec2-3-217-251-77.compute-1.amazonaws.com:5432/diqqotinlaii4";
+        String url = "jdbc:postgres://lirbgwsmobmsbb:45a31ccead123ab78ba6ef0a719744999157d94ef5c0ea69703c05d81095fc62@ec2-3-217-251-77.compute-1.amazonaws.com:5432/diqqotinlaii4";
         Properties props = new Properties();
         props.setProperty("user","lirbgwsmobmsbb");
         props.setProperty("password","45a31ccead123ab78ba6ef0a719744999157d94ef5c0ea69703c05d81095fc62");
