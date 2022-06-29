17c2ec7710d07e5bc7e6e891fd1df46bd34e2727
Pedro Martire <pmartire@pmartir-ltm3am9.internal.salesforce.com>
2022-06-29 01:41:16 -0300

    commit 14

--- src/main/java/com/tccuva1/tccuva/DatabaseConnection.java
+++ src/main/java/com/tccuva1/tccuva/DatabaseConnection.java
@@ -7,14 +7,8 @@ import java.util.Properties;
 public class DatabaseConnection {
 
 
-    public static void createConnection() throws ClassNotFoundException, URISyntaxException{
-        /* 
-        String url = "jdbc:postgres://lirbgwsmobmsbb:45a31ccead123ab78ba6ef0a719744999157d94ef5c0ea69703c05d81095fc62@ec2-3-217-251-77.compute-1.amazonaws.com:5432/diqqotinlaii4";
-        Properties props = new Properties();
-        props.setProperty("user","lirbgwsmobmsbb");
-        props.setProperty("password","45a31ccead123ab78ba6ef0a719744999157d94ef5c0ea69703c05d81095fc62");
-        props.setProperty("ssl","true");
-        */
+    public static Connection createConnection() throws ClassNotFoundException, URISyntaxException{
+
         try {
 
             URI dbUri = new URI(System.getenv("DATABASE_URL"));
@@ -23,11 +17,12 @@ public class DatabaseConnection {
             String password = dbUri.getUserInfo().split(":")[1];
             String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
         
-            DriverManager.getConnection(dbUrl, username, password);
+            return DriverManager.getConnection(dbUrl, username, password);
             
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
+
             e.printStackTrace();
+            return null;
         }
 
     }
