a09a20910fac8273bd1d94c864cd8e35d66c5ecb
Pedro Martire <pmartire@pmartir-ltm3am9.internal.salesforce.com>
2022-07-04 17:40:26 -0300

    make it better

--- src/main/java/com/tccuva1/tccuva/LeadController.java
+++ src/main/java/com/tccuva1/tccuva/LeadController.java
@@ -22,8 +22,8 @@ public class LeadController {
         try {
             resultSet = DatabaseConnection.makeQuery(1,"SELECT \"Nome\", \"Email\", \"Cod_Lead\", \"Idade\", \"Canal\", \"Data_Criacao\" FROM public.\"Lead\" ORDER BY \"Cod_Lead\" DESC;");
 
-            resultSet.next();
-            System.out.println("Here 1: " + resultSet.getString(1));
+            //resultSet.next();
+            //System.out.println("Here 1: " + resultSet.getString(1));
 
             while(resultSet.next()){
                 leads.add( new Lead(resultSet.getString(1), resultSet.getString(2), 
