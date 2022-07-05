ab56d74035863b277adb7738a915e33b61e00a89
Pedro Martire <pmartire@pmartir-ltm3am9.internal.salesforce.com>
2022-07-05 05:22:00 -0300

    make it better

--- src/main/java/com/tccuva1/tccuva/AtendimentoController.java
+++ src/main/java/com/tccuva1/tccuva/AtendimentoController.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class AtendimentoController {
 
     public static List <Atendimento> getAllAtendimentos(){
